977cca8f3c8dc148cde31dbd1a087e449e0fa51d
Victor Ukafor <victorukafor@gmail.com>
2018-12-21 12:44:00 +0100

    feat(landing-page): setup file structure
    - set the proper file structure for the app
    - configures linting
    
    [delivers #162267873]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+import reducers from './reducers';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger;
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk, promise, logger),
+);
 
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const reducers = combineReducers({
+  users: ['users'],
+});
+
+export default reducers;
