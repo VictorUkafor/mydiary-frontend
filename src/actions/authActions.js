5be433e8e33ed45880cce12ea1f5db9253c23e9f
Victor Ukafor <victorukafor@gmail.com>
2019-03-05 13:43:49 +0100

    fix proxy issues

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import * as types from './actionTypes';
+import {} from 'dotenv/config';
 
 export const signupUser = (body, callback) => async (dispatch) => {
   try {
-    const response = await axios.post('/auth/signup', body);
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/signup`, body);
     dispatch({
       type: types.SIGNUP_USER,
       payload: response.data.success,
@@ -20,7 +21,7 @@ export const signupUser = (body, callback) => async (dispatch) => {
 
 export const loginUser = (body, callback) => async (dispatch) => {
   try {
-    const response = await axios.post('/auth/login', body);
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login', body);
     localStorage.setItem('token', response.data.token);
     dispatch({
       type: types.LOGIN_USER,
